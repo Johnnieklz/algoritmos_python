ecbd0a7e0fd88ff1f7ba3b01a84a7422cfff8734
Johnnieklz <159908309+Johnnieklz@users.noreply.github.com>
2025-04-11 21:24:17 -0300

    atualização tarefas

---
+++ listas/tarefalistas1.py
@@ -0,0 +1,6 @@
+vetor = [1, 2, 3, 4, 5]
+
+print(vetor[0])
+
+for numero in vetor:
+    print(numero)
---
+++ listas/tarefalistas2.py
@@ -0,0 +1,9 @@
+num = []
+
+for i in range(10):
+    n = float(input("Digite um número: "))
+    num.append(n)
+
+num.reverse()
+print("O reverso é:", num)
+
